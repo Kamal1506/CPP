56062cb6b661c0134b8928fcf8c6d56283bd4929
Kamal Arumugam <kamalak1506@gmail.com>
2026-09-08 19:08:14 +0530

    Implement palindrome checker in palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    int left = 0;
+    int right = s.size()-1;
+    
+    while(left < right){
+        if(s[left] != s[right]){
+            cout << "Not a Palindrome";
+            return 0;
+        }
+        left++;
+        right--;
+    }
+    cout << "Palindrome";
+    return 0;
+}
+//palindrome
