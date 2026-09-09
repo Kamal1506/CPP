c7d0ef0a9234f8cf4a442ec2e999865f5e446ae5
Kamal Arumugam <kamalak1506@gmail.com>
2026-09-09 11:50:04 +0530

    Improve duplicate removal logic in removeDuplicates.cpp
    Replaced the original duplicate removal logic with a more efficient approach using a boolean array to track seen characters.

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -11,12 +11,31 @@ int main(){
     
     string res;
     
-    for(int i = 0; i<word.size(); i++){
-        int index = word.find(word[i]);
-        if(index == i){
-            res.push_back(word[i]);
+    // for(int i = 0; i<word.size(); i++){
+    //     int index = word.find(word[i]);
+    //     if(index == i){
+    //         res.push_back(word[i]);
+    //     }
+    // }
+
+    bool seen[256] = false;
+
+    for(char ch : word){
+        if(!seen[(unsigned char)ch]){
+            res.push_back(ch);
+            seen[(unsigned char)ch] = true;
         }
     }
     
     cout << res;
 }
+
+// We are using the character's numeric value as an array index.
+// For normal ASCII characters:
+// 'a' → 97
+// 'A' → 65
+// '0' → 48
+// So conceptually:
+// seen[(unsigned char)'a']
+
+// becomes : seen[97]
